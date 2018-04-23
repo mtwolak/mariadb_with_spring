e7fc78044c526932d15d8ed3401d1b145b6da106
Marcin Wolak <mtwolak@gmail.com>
2018-04-23 17:06:48 +0200

    Separate docker files for each image

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
-gradle build -p spring/
-docker-compose up -d
+docker network create myNetwork
+docker build -t spring spring/
+docker build -t maria-sep sql/
+docker run -d --name mariadb --network=myNetwork maria-sep
+docker run -d --network=myNetwork -p 8080:8080 --restart always spring
